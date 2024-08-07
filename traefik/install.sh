4d44b239f96869967a45559ee7291517f864d61e
Sebastian Brosch <code@sebastianbrosch.de>
2024-08-07 23:28:19 +0200

    update to traefik 3.1.2

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,8 +1,8 @@
 # set the ip or domain to access the reverse proxy.
-export SERVER_IP=proxy.example.com
+export SERVER_DOMAIN=proxy.example.org
 export TRAEFIK_USERNAME=traefik
-export TRAEFIK_PASSWORD=password
-TRAEFIK_PASSWORD=$(openssl passwd -apr1 $TRAEFIK_PASSWORD)
+password=password
+export TRAEFIK_PASSWORD=$(openssl passwd -apr1 ${password})
 
 # create network and stack.
 docker network inspect traefik-public >/dev/null 2>&1 || docker network create --driver=overlay traefik-public
