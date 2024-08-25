098fd6b37a3c447ea9a71204f75236a3300b07b1
Sebastian Brosch <code@sebastianbrosch.de>
2024-08-26 00:05:50 +0200

    updated traefik installation

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,9 +1,15 @@
-# set the ip or domain to access the reverse proxy.
-export SERVER_DOMAIN=proxy.example.org
-export TRAEFIK_USERNAME=traefik
-password=password
-export TRAEFIK_PASSWORD=$(openssl passwd -apr1 ${password})
+#!/bin/bash
 
-# create network and stack.
+# change these values to your needs.
+export SERVER_DOMAIN=proxy.example.com
+export TRAEFIK_USERNAME=traefik
+export TRAEFIK_PASSWORD=traefik
+
+# the password have to be encrypted.
+export TRAEFIK_PASSWORD=$(openssl passwd -apr1 ${TRAEFIK_PASSWORD})
+
+# create the network of the proxy.
 docker network inspect traefik-public >/dev/null 2>&1 || docker network create --driver=overlay traefik-public
+
+# deploy the docker stack.
 docker stack deploy -c docker-compose.yml traefik
