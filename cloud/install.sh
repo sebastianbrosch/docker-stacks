93b913a18e416e4f7d44e93d08b6fc81a936d273
Sebastian Brosch <code@sebastianbrosch.de>
2024-08-25 23:50:07 +0200

    updated cloud installation

--- cloud/install.sh
+++ cloud/install.sh
@@ -1,16 +1,23 @@
-export SERVER_DOMAIN=cloud.sebastianbrosch.de
-export ADMIN_PASSWORD=admin
+#!/bin/bash
+
+# change these values to your needs.
+export SERVER_DOMAIN=cloud.example.com
+export ADMIN_PASSWORD=XY6AGjRh4qvkTLy0e1BO
 export SERVICE_ACCOUNT_ID=$(uuidgen)
+
+# the credentials for the service account are generated radomly.
+# you don't have to know the credentials for the service account.
 password=$(openssl passwd -6 $(uuidgen))
 export SERVICE_ACCOUNT_SECRET=${password:25:25}
 
-# print generated information
+# print the credentials of the service account.
 echo "Service Account ID: $SERVICE_ACCOUNT_ID"
 echo "Service Account Secret: $SERVICE_ACCOUNT_SECRET"
 
-# create the folders for configuration and data
+# create the directories for configuration and data.
+# you also have to set the ownership of these directories.
 mkdir -p config data
 chown 1000:1000 config data
 
-# deploy the docker stack
+# deploy the docker stack.
 docker stack deploy -c docker-compose.yml owncloud
