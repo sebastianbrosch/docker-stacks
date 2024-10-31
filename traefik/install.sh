b302b20c70fd105337fd56fef75dc29b58d944b5
Sebastian Brosch <code@sebastianbrosch.de>
2024-10-31 22:33:39 +0100

    improved install script for traefik

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,13 +1,60 @@
 #!/bin/bash
 
-# load all the environment variables from .env file.
-set -a; source .env; set +a
+# set the version of the various services.
+export VERSION_TRAEFIK=3.2.0
 
-# the password have to be encrypted.
-export TRAEFIK_PASSWORD=$(openssl passwd -apr1 ${TRAEFIK_PASSWORD})
+# export all environment variables from external .env file.
+# the docker compose command can use these variables for substitution.
+set -a;
+source <(sed -e "s/\r//" .env);
+set +a;
 
-# create the network of the proxy.
-docker network inspect traefik-public >/dev/null 2>&1 || docker network create --driver=overlay traefik-public
+# set the password for basic authentication.
+TRAEFIK_PASSWORD=$(openssl passwd -apr1 "${TRAEFIK_PASSWORD:-traefik}")
+export TRAEFIK_PASSWORD
 
-# deploy the docker stack.
-docker stack deploy -c docker-compose.yml traefik
+debug() {
+  docker stack config -c docker-compose.yml
+}
+
+help() {
+  echo "This script installs Traefik with all prerequisites."
+  echo ""
+  echo "Usage: bash install.sh [options...]"
+  echo ""
+  echo "The following options are available:"
+  printf "%4s, %-10s %-70s\n" "-d" "--debug" "Shows the configuration for the Docker stack."
+  printf "%4s, %-10s %-70s\n" "-h" "--help" "Shows this help."
+  printf "%4s, %-10s %-70s\n" "-i" "--install" "Installs the Traefik stack with all prerequisites."
+  printf "%4s, %-10s %-70s\n" "-v" "--version" "Shows the version of the services."
+}
+
+install() {
+
+  # create the network of the proxy.
+  docker network inspect traefik-public >/dev/null 2>&1 || docker network create --driver=overlay traefik-public
+
+  # deploy the docker stack.
+  docker stack deploy -c docker-compose.yml traefik
+}
+
+version() {
+  printf "%-15s %-10s\n" "Traefik:" "${VERSION_TRAEFIK}"
+}
+
+if [[ "$#" == "0" ]]; then
+  help
+else
+  while [[ "$#" != "0" ]]; do
+    if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+      help
+    elif [[ "$1" == "-d" ]] || [[ "$1" == "--debug" ]]; then
+      debug
+    elif [[ "$1" == "-i" ]] || [[ "$1" == "--install" ]]; then
+      install
+    elif [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
+      version
+    fi
+    shift
+  done
+fi
